07e78fb13f1d7c4d65a00c107b1d68c68ebbf547
ccanozerr <ccanozerr@gmail.com>
2021-04-17 16:26:26 +0300

    initial commit

---
+++ fr-microservice/src/main/java/com/ccanozerr/fr/FrMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ccanozerr.fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrMicroserviceApplication.class, args);
+	}
+
+}
---
+++ fr-microservice/src/test/java/com/ccanozerr/fr/FrMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccanozerr.fr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
